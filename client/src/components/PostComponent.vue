23a141b6bb9209439edcb0ee74e50138fd26e4c3
Joshua <joshchaffy@gmail.com>
2024-02-11 11:31:01 -0500

    Initial front end

---
+++ client/src/components/PostComponent.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class = "container">
+    <h1>Latest Posts</h1>
+    <!-- CREATE POST HERE -->
+    <hr>
+    <p class="error" v-if="error">{{ error }}</p>
+    <div class = "posts-container">
+      <div class="post"
+        v-for="(post, index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+      >
+        <p class="date">
+          {{
+            (new Date(post.createdAt).getMonth() + 1) + '/' +
+            new Date(post.createdAt).getDate() + '/' +
+            new Date(post.createdAt).getFullYear()
+          }}
+        </p>
+        <p class="text">{{ post.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostService from '../PostService';
+
+export default {
+  name: 'PostComponent',
+  data() {
+    return {
+      posts: [],
+      error: '',
+      text: ''
+    }
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div.container {
+  margin: 0 auto;
+  width: 50%;
+  text-align: left;
+}
+p.error {
+  border: 1px solid red;
+  background-color: pink;
+  padding: 10px;
+  margin-bottom: 15px;
+}
+
+div.post{
+  position: relative;
+  border: 1px solid green;
+  background-color: lightgreen;
+  padding: 10px 10px 30px 10px;
+  margin-bottom: 15px;
+}
+
+div.created-at {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  font-size: 10px;
+  color: grey;
+  padding: 5px;
+  background-color: white;
+  border: 1px solid grey;
+  border-radius: 5px;
+}
+
+p.text {
+  font-size: 20px;
+  color: black;
+  margin-bottom: 10px;
+}
+
+</style>
