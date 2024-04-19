ee82b32ab7426331397f5cee84a638516a4c68bb
Joshua <joshchaffy@gmail.com>
2024-04-19 00:33:12 -0400

    Adding espCode and Python

---
+++ esp32Code/sign2.ino
@@ -0,0 +1,466 @@
+/*
+  Two way communication example: https://RandomNerdTutorials.com/esp-now-two-way-communication-esp32/
+  
+*/
+
+//for esp 1
+
+#include <esp_now.h>
+#include <WiFi.h>
+#include <ESP32Servo.h>
+
+#define onBoard 2
+#define echoPin2 22
+#define trigPin2 23
+#define RXp2 16
+#define TXp2 17
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress1[] = {0xA0, 0xA3, 0xB3, 0xED, 0xA5, 0x60}; // esp1
+uint8_t broadcastAddress2[] = {0xA0, 0xA3, 0xB3, 0xED, 0xB7, 0x1C}; // esp3
+
+//define Servo object
+Servo myServo;
+int Servo = 13;
+
+//local variables
+String buffer = "";
+bool prevRedRed = false;
+int carsInLane = 0;
+bool prevIsSlow = false;
+bool isTimerGoing = false;
+bool isTimerYellowGoing = false;
+bool isTimerRedGoing = false;
+bool prevCarCheck3 = false;       //car entering
+bool currentCarCheck3 = false;
+bool prevCarCheck4 = false;       // car leaving
+bool currentCarCheck4 = false;
+unsigned long prevMillis1 = 0;
+unsigned long prevMillis2 = 0;
+unsigned long prevMillis3 = 0;
+unsigned long timer = 0; // for when the lane is congested
+unsigned long timerYellow = 0; // for when the light just turned yellow
+unsigned long timerRed = 0;  // for when the light just turned red
+unsigned long timerBeforeYellow = 0; // for before the light can turn yellow
+const long interval1 = 250; // .25 sec for car count
+const long interval2 = 250; // .25 sec for car subtract
+const long interval3 = 50; // .05 sec for sending radio (50 for fastest) (1000 for testing)
+
+const long interval4 = 3000; // timer for when there is constant traffic flow on both sides
+const long interval5 = 5000; // timer for when the light just turned yellow
+const long interval6 = 3000; // timer for when the light just turned red
+const long interval7 = 3000; // timer for before the light can turn yellow incase a car runs the opposite red
+
+// Define variables to be sent
+int carsEntering;
+int carsLeaving;
+int signCount;
+bool isSlow = false;
+bool isCarWaiting = false;
+bool isCarApproaching = false;
+bool isGoToRed = false;
+
+// Define variables to store incoming readings
+int incomingCarsEntering;
+int incomingCarsLeaving;
+bool incomingIsSlow;
+bool incomingIsCarWaiting;
+bool incomingIsCarApproaching;
+bool incomingIsGoToRed;
+bool incomingIsRedRed;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    int carsinlane;
+    int id;
+    int carsentering;
+    int carsleaving;
+    int signCount;
+    bool slw;
+    bool iscarwaiting;
+    bool iscarapproaching;
+    bool isgotored;
+} struct_message;
+
+typedef struct struct_message2 {
+  bool redred;
+} struct_message2;
+
+// Create a struct_message to hold sensor readings
+struct_message trafficSignReadings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+// a struct_message2 to hold incoming readings
+struct_message2 incomingRemote;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  //Serial.print("\r\nLast Packet Send Status:\t");
+  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    //success = "Delivery Success :)";
+  }
+  else{
+    //success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  //Serial.print("Bytes received: ");
+  //Serial.println(len);
+   if(len >= 2){
+   incomingCarsEntering = incomingReadings.carsentering;
+   incomingCarsLeaving = incomingReadings.carsleaving;
+   incomingIsSlow = incomingReadings.slw;
+   incomingIsCarWaiting = incomingReadings.iscarwaiting;
+   incomingIsCarApproaching = incomingReadings.iscarapproaching;
+   incomingIsGoToRed = incomingReadings.isgotored;
+  }
+  else {
+    memcpy(&incomingRemote, incomingData, sizeof(incomingRemote));
+    incomingIsRedRed = incomingRemote.redred;
+  }
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, RXp2, TXp2);
+
+  pinMode(onBoard, OUTPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+
+  /* servo */
+  myServo.attach(Servo);
+  myServo.write(0);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    //Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // register second peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+       
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if(incomingIsRedRed){
+
+    carsInLane = 0;
+    // Set values to send
+    trafficSignReadings.id = 2;
+    trafficSignReadings.slw = isSlow;
+    trafficSignReadings.carsentering = carsEntering;
+    trafficSignReadings.carsleaving = carsLeaving;
+    trafficSignReadings.iscarapproaching = isCarApproaching;
+    trafficSignReadings.iscarwaiting = isCarWaiting;
+    trafficSignReadings.isgotored = isGoToRed;
+    trafficSignReadings.carsinlane = carsInLane;
+
+    // Send message via ESP-NOW
+    if (currentMillis - prevMillis3 >= interval3){
+
+      esp_err_t result = esp_now_send(0, (uint8_t *) &trafficSignReadings, sizeof(trafficSignReadings));
+      prevMillis3 = currentMillis;
+        if (result == ESP_OK) {
+          //Serial.println("Sent with success");
+        }
+        else {
+          //Serial.println("Error sending the data");
+        }
+    }
+  }
+
+  if(!incomingIsRedRed && prevRedRed && isSlow){
+    moveServoSlow();
+  }
+
+  if(!incomingIsRedRed){
+    // If the sign is yellow/slow
+    if(isSlow){
+
+      //Outside sensor
+      if (currentMillis - prevMillis1 >= interval1) {
+        prevMillis1 = currentMillis;   
+        carDetection1();  
+      }
+      //Inside sensor
+      if (currentMillis - prevMillis2 >= interval2) {
+        UltrasonicRead3();
+        prevMillis2 = currentMillis;
+      }
+      carsInLane = carsEntering - incomingCarsLeaving;
+    }
+    
+    // If the sign is red/stop and the light did not just turn red
+    else if ((currentMillis - timerRed >= interval6) && !isSlow && (!prevIsSlow || incomingIsSlow) ) { // added && (!prevIsSlow || incomingIsSlow)
+
+      //Outside sensor
+      if (currentMillis - prevMillis1 >= interval1) {
+        prevMillis1 = currentMillis;
+        carDetection2();     
+      }
+      //Inside sensor
+      if (currentMillis - prevMillis2 >= interval2) {
+        UltrasonicRead4();
+        prevMillis2 = currentMillis;
+      }
+      carsInLane = incomingCarsEntering - carsLeaving;
+        if(prevIsSlow && (!incomingIsSlow) ){
+          carsInLane =  carsEntering - incomingCarsLeaving;
+        }
+      isTimerRedGoing = false;
+    }
+
+    // If the light just turned red/stop continue to count cars entering the lane for 3 seconds
+    // In case a car runs the red light.
+    else if(!isSlow && prevIsSlow && !incomingIsSlow){  //was (!isSlow && isTimerRedGoing)
+
+      if (currentMillis - prevMillis2 >= interval2) {
+        UltrasonicRead3();
+        prevMillis2 = currentMillis;
+      }
+      carsInLane =  carsEntering - incomingCarsLeaving;
+    }
+
+    // Set values to send
+    trafficSignReadings.id = 2;
+    trafficSignReadings.slw = isSlow;
+    trafficSignReadings.carsentering = carsEntering;
+    trafficSignReadings.carsleaving = carsLeaving;
+    trafficSignReadings.iscarapproaching = isCarApproaching;
+    trafficSignReadings.iscarwaiting = isCarWaiting;
+    trafficSignReadings.isgotored = isGoToRed;
+    trafficSignReadings.carsinlane = carsInLane;
+    
+    // Send message via ESP-NOW
+    if (currentMillis - prevMillis3 >= interval3){
+
+      esp_err_t result = esp_now_send(0, (uint8_t *) &trafficSignReadings, sizeof(trafficSignReadings));
+      prevMillis3 = currentMillis;
+        if (result == ESP_OK) {
+          //Serial.println("Sent with success");
+        }
+        else {
+          //Serial.println("Error sending the data");
+        }
+    }
+    
+    updateSign();
+    delay(100);
+    prevRedRed = false;
+  }
+
+  // If the light is yellow, turn to red and reset carsInLane
+  if (incomingIsRedRed && !prevRedRed && isSlow) {
+      moveServoStop();
+      prevRedRed = incomingIsRedRed;
+      carsEntering = 0;
+      carsLeaving = 0;
+      carsInLane = 0;
+  }
+
+  // If the light is already red reset carsInLane
+  else if (incomingIsRedRed && !prevRedRed && !isSlow) {
+      prevRedRed = incomingIsRedRed;
+      carsEntering = 0;
+      carsLeaving = 0;
+      carsInLane = 0;
+  }
+}
+
+
+/* When yellow "car is approaching" */  
+void carDetection1(){
+        String incomingMessage = Serial2.readStringUntil('\n');
+        incomingMessage.trim();
+        Serial.print("Message Received: ");
+        Serial.println(incomingMessage);
+        if (incomingMessage == "T") {
+            isCarWaiting = true;
+            digitalWrite(onBoard, HIGH);
+        } else if(incomingMessage == "F"){
+            isCarWaiting = false;
+            digitalWrite(onBoard, LOW);
+        }
+}
+
+/* When red say "car is waiting"*/
+void carDetection2(){
+    String incomingMessage = Serial2.readString();
+    incomingMessage.trim();
+    Serial.print("Message Received: ");
+    Serial.println(incomingMessage);
+    if(incomingMessage == "True")
+    {
+      isCarWaiting = true;
+      digitalWrite(onBoard, HIGH);
+    }
+    else if(incomingMessage == "F"){
+      isCarWaiting = false;
+      digitalWrite(onBoard, LOW);
+    }
+}
+
+/*When yellow "car entered lane"*/
+void UltrasonicRead3(){
+    long duration, distance;
+    digitalWrite(trigPin2, LOW);  
+    delayMicroseconds(2); 
+    digitalWrite(trigPin2, HIGH);
+    delayMicroseconds(10); 
+    digitalWrite(trigPin2, LOW);
+    duration = pulseIn(echoPin2, HIGH);
+    distance = (duration/2) / 29.1;
+    //Serial.println(distance);
+    if ((distance < 20) && (distance > 0)) { 
+      digitalWrite(onBoard, HIGH);
+      currentCarCheck3 = true;                   
+      if (currentCarCheck3 && !prevCarCheck3) {
+        signCount++;
+        carsEntering++;
+        Serial.print("Cars Entering Lane: ");
+        Serial.println(carsEntering);
+      }
+    } 
+    else {
+      digitalWrite(onBoard, LOW);
+      currentCarCheck3 = false;
+    }
+    prevCarCheck3 = currentCarCheck3;
+}
+
+/* When red say "car is leaving"*/
+void UltrasonicRead4(){
+    long duration, distance;
+    digitalWrite(trigPin2, LOW);  
+    delayMicroseconds(2); 
+    digitalWrite(trigPin2, HIGH);
+    delayMicroseconds(10); 
+    digitalWrite(trigPin2, LOW);
+    duration = pulseIn(echoPin2, HIGH);
+    distance = (duration/2) / 29.1;
+    //Serial.println(distance);
+    if ((distance < 20) && (distance > 0)) { 
+      digitalWrite(onBoard, HIGH);
+      currentCarCheck4 = true; 
+      //Serial.print("Cars entering Lane: ");
+      //Serial.println(incomingCarsEntering);                  
+      if (currentCarCheck4 && !prevCarCheck4 && (incomingCarsEntering > carsLeaving)) {
+        carsLeaving++;
+        Serial.print("Cars Leaving: ");
+        Serial.println(carsLeaving);
+      }
+    } 
+    else {
+      digitalWrite(onBoard, LOW);
+      currentCarCheck4 = false;
+    }
+    prevCarCheck4 = currentCarCheck4;
+}
+
+void moveServoStop() {
+    myServo.write(0);
+    delay(500);
+    myServo.detach();
+}
+
+void moveServoSlow() {
+    myServo.write(180);
+    delay(500);
+    myServo.detach();
+}
+
+void updateSign(){
+
+unsigned long currentMillis = millis();
+
+
+// If a car is waiting at the red light and the other light is yellow
+  if(isCarWaiting && incomingIsSlow){
+
+    prevIsSlow = isSlow;
+
+    // If the lane is occupied
+    if((carsInLane > 0) && !isTimerGoing){
+      timer = millis();
+      isTimerGoing = true;
+      Serial.println("Timer Started");
+      Serial.println("Waiting for car to exit lane");
+    }
+
+    // If the timer ended or the cars left the lane
+    if((currentMillis - timer >= interval4) || (carsInLane == 0)){
+      isTimerGoing = false;
+      isGoToRed = true;
+      timerBeforeYellow = millis();
+    }
+  }
+
+  // If the timer ends and there is still a car in the lane
+  // Then the other light is red and a car is waiting here till all the cars leave the lane
+  // Must wait 3 seconds because a car at the other end might run the light
+  // Need the !prevIsSlow because This state can only happen during prev red and current red
+  if((currentMillis - timerBeforeYellow >= interval7) && isCarWaiting && !incomingIsSlow && !prevIsSlow && (carsInLane == 0) && !isSlow){
+    moveServoSlow();
+    carsEntering = 0;
+    isGoToRed = false;
+    isSlow = true;
+    timerYellow = millis();
+    Serial.print("Timer started at: ");
+    Serial.println(timerYellow);
+    isCarWaiting = false;
+  }
+
+  //In order for the light to go to red, 5 seconds must have passed from when the light first turned yellow
+  if((currentMillis - timerYellow >= interval5) && incomingIsGoToRed && isSlow){
+    moveServoStop();
+    //Start timer for turning red, This is because we will continue to red cars entering the lane
+    //In case cars run the red light.
+    timerRed = millis();
+    isTimerRedGoing = true;
+    
+    prevIsSlow = isSlow;
+    isSlow = false;
+    carsLeaving = 0;
+    Serial.println("going to red");
+  }
+}
---
+++ objectDetection/dualCam.py
@@ -0,0 +1,190 @@
+import threading
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import urllib.request
+import numpy as np
+import requests
+from http.client import IncompleteRead
+from multiprocessing import Process
+from threading import Thread
+
+
+
+# Replace with the IP address of your ESP32 device
+esp32_ip1 = '192.168.8.227'
+esp32_ip2 = '192.168.8.230'
+
+# URL for the WebServer
+url1 = f"http://{esp32_ip1}:81/"
+url2 = f"http://{esp32_ip2}:81/"
+
+# URL for the AsyncWebServer
+stream_url1 = f"http://{esp32_ip1}:80/cam-hi.jpg"
+stream_url2 = f"http://{esp32_ip2}:80/cam-hi.jpg"
+
+# Create a VideoCapture object
+cap1 = cv2.VideoCapture(stream_url1)
+cap2 = cv2.VideoCapture(stream_url2)
+
+# Check if the IP camera stream is opened successfully
+if not cap1.isOpened():
+    print("Failed to open the IP camera stream")
+    exit()
+if not cap2.isOpened():
+    print("Failed to open the IP camera stream")
+    exit()
+
+model = YOLO("yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", 
+              "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork",
+              "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog",
+              "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv",
+              "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink",
+              "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush",
+             ]
+
+prev_message_str = None
+
+car_detected = False
+mask = cv2.imread("images/espCam1Mask.png")
+
+def send_post_request(url, data):
+    try:
+        requests.post(url, data=data)
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to send POST request: {e}")
+
+def run_detection1():
+    global cap, model, classNames, prev_message_str, car_detected
+    while True:
+        img_resp = urllib.request.urlopen(stream_url1)
+
+        try:
+            imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        except IncompleteRead as e:
+            imgnp = np.array(bytearray(e.partial), dtype=np.uint8)
+
+        img = cv2.imdecode(imgnp,-1)
+        cv2.line(img, (400, 0), (400, 600), (0, 255, 0), 2)
+        imgRegion = cv2.bitwise_and(img, mask)
+
+        results = model(imgRegion, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+
+                # Bounding Box
+                x1,y1,x2,y2 = box.xyxy[0]
+                x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
+
+                w, h = x2-x1, y2-y1
+
+                # Confidence
+                conf = math.ceil((box.conf[0] *100))/100   #round confidence to 2 decimal places
+
+                # Class Name
+                cls = int(box.cls[0])
+                currentClass = classNames[cls]
+
+                # Draw only vehicles
+                if currentClass == "car" and conf > 0.3:
+                    cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)), 3, 3, (255,255,255), (0,255,0))
+                    cvzone.cornerRect(img, (x1,y1,w,h), 30, 5, 1, (0, 255, 0), (0, 255, 0))
+                    message_str = "T"
+                    car_detected = True
+
+                # Convert the image to JPEG
+                ret, jpeg = cv2.imencode('.jpg', img)
+                frame = jpeg.tobytes()
+
+                # Display the resulting frame
+                cv2.imshow('Frame1', img)
+
+                # Break the loop on 'q' key press
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        if not car_detected:
+            message_str = "F"
+
+        if message_str != prev_message_str:
+            data = {"value": message_str}
+            Thread(target=send_post_request, args=(url1, data)).start()
+            prev_message_str = message_str
+
+        car_detected = False
+
+def run_detection2():
+    global cap, model, classNames, prev_message_str, car_detected
+    while True:
+        img_resp = urllib.request.urlopen(stream_url2)
+
+        try:
+            imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        except IncompleteRead as e:
+            imgnp = np.array(bytearray(e.partial), dtype=np.uint8)
+
+        img = cv2.imdecode(imgnp,-1)
+        cv2.line(img, (400, 0), (400, 600), (0, 255, 0), 2)
+        imgRegion = cv2.bitwise_and(img, mask)
+
+        results = model(imgRegion, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+
+                # Bounding Box
+                x1,y1,x2,y2 = box.xyxy[0]
+                x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
+
+                w, h = x2-x1, y2-y1
+
+                # Confidence
+                conf = math.ceil((box.conf[0] *100))/100   #round confidence to 2 decimal places
+
+                # Class Name
+                cls = int(box.cls[0])
+                currentClass = classNames[cls]
+
+                # Draw only vehicles
+                if currentClass == "car" and conf > 0.3:
+                    cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)), 3, 3, (255,255,255), (0,255,0))
+                    cvzone.cornerRect(img, (x1,y1,w,h), 30, 5, 1, (0, 255, 0), (0, 255, 0))
+                    message_str = "T"
+                    car_detected = True
+
+                # Convert the image to JPEG
+                ret, jpeg = cv2.imencode('.jpg', img)
+                frame = jpeg.tobytes()
+
+                # Display the resulting frame
+                cv2.imshow('Frame2', img)
+
+                # Break the loop on 'q' key press
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        if not car_detected:
+            message_str = "F"
+
+        if message_str != prev_message_str:
+            data = {"value": message_str}
+            Thread(target=send_post_request, args=(url2, data)).start()
+            prev_message_str = message_str
+
+        car_detected = False
+
+if __name__ == '__main__':
+    # Run the detection functions in separate threads
+    p1 = threading.Thread(target=run_detection1)       #Process(target=run_detection1)
+    p2 = threading.Thread(target=run_detection2)       #Process(target=run_detection2)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
